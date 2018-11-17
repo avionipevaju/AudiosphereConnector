ab849db1ffb710ceafb123c257737208ac751410
avionipevaju <antic.nikola@outlook.com>
2018-11-17 20:02:55 +0100

    Improved http operation failed processor.

--- m-py-connector/src/main/java/org/avionipevaju/moody/py/connector/processor/exception/HttpOperationFailedExceptionProcessor.java
+++ m-py-connector/src/main/java/org/avionipevaju/moody/py/connector/processor/exception/HttpOperationFailedExceptionProcessor.java
@@ -1,5 +1,6 @@
 package org.avionipevaju.moody.py.connector.processor.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
 import org.apache.camel.http.common.HttpOperationFailedException;
 import org.avionipevaju.moody.py.connector.dto.ExecutionResponse;
@@ -7,13 +8,21 @@ import org.avionipevaju.moody.py.connector.processor.AbstractProcessor;
 
 public class HttpOperationFailedExceptionProcessor extends AbstractProcessor {
 
+    private ObjectMapper objectMapper;
+
     @Override
     public void process(Exchange exchange) throws Exception {
         HttpOperationFailedException exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, HttpOperationFailedException.class);
-        ExecutionResponse executionResponse = new ExecutionResponse();
-        executionResponse.setStatus(String.valueOf(exception.getStatusCode()));
-        executionResponse.setDescription(String.valueOf(exception.getStatusText()));
+        ExecutionResponse executionResponse = getObjectMapper().readValue(exception.getResponseBody(), ExecutionResponse.class);
         logAsJson(exchange.getIn().getHeaders(), executionResponse);
         exchange.getOut().setBody(executionResponse);
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
 }
