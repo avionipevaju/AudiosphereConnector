7ff554d224c08fc97f6c5e3e7608e2817518efec
avionipevaju <antic.nikola@outlook.com>
2019-01-12 15:35:14 +0100

    Updated TwitterRouteBuilder to use GenericRouteBuilder. Removed tests. Removed TwitterScheduler.

--- m-py-connector/src/main/java/org/avionipevaju/moody/py/connector/route/twitter/TwitterRouteBuilder.java
+++ m-py-connector/src/main/java/org/avionipevaju/moody/py/connector/route/twitter/TwitterRouteBuilder.java
@@ -1,47 +1,13 @@
 package org.avionipevaju.moody.py.connector.route.twitter;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.http.common.HttpOperationFailedException;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.avionipevaju.moody.py.connector.dto.twitter.TwitterRequest;
 import org.avionipevaju.moody.py.connector.dto.twitter.TwitterResponse;
-import org.avionipevaju.moody.py.connector.route.AbstractRouteBuilder;
+import org.avionipevaju.moody.py.connector.route.GenericRouteBuilder;
 
-public class TwitterRouteBuilder extends AbstractRouteBuilder {
+public class TwitterRouteBuilder extends GenericRouteBuilder<TwitterRequest, TwitterResponse> {
 
-    @Override
-    public void configure() throws Exception {
-
-        rest().post("content/post").id("postContentRoute")
-                .type(TwitterRequest.class).consumes("application/json")
-                .outType(TwitterResponse.class).produces("application/json")
-                .description("")
-                .route()
-                .doTry()
-                    .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                    .process(getRequestProcessor())
-                    .marshal().json(JsonLibrary.Jackson)
-                    .to(getEndpoint())
-                    .unmarshal().json(JsonLibrary.Jackson, TwitterResponse.class)
-                    .process(getResponseProcessor())
-                .doCatch(HttpOperationFailedException.class)
-                    .process(getHttpOperationFailedExceptionProcessor())
-                .doCatch(Throwable.class)
-                    .process(getExceptionHandlingProcessor());
-
-        from("direct:post").id("postContentFromInstagramRoute")
-                .doTry()
-                    .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                    .process(getRequestProcessor())
-                    .marshal().json(JsonLibrary.Jackson)
-                    .to(getEndpoint())
-                    .unmarshal().json(JsonLibrary.Jackson, TwitterResponse.class)
-                    .process(getResponseProcessor())
-                .doCatch(HttpOperationFailedException.class)
-                    .process(getHttpOperationFailedExceptionProcessor())
-                .doCatch(Throwable.class)
-                    .process(getExceptionHandlingProcessor());
-
+    public TwitterRouteBuilder(String restPath, String routeStartPath) {
+        super(TwitterRequest.class, TwitterResponse.class, restPath, routeStartPath);
     }
 
 }
